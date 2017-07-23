766fe900aeb942013169f68ac178595e848fb9fb
Duy Dao <28035859+DuyTheDao@users.noreply.github.com>
2017-07-23 11:40:46 -0400

    Create review_exercise_p59.py
    Review exercises:
    
    1. Write a for loop that prints out the integers 2 through 10, each on a new line, by
    using the range() function
    
    2. Use a while loop that prints out the integers 2 through 10 (Hint: you'll need to
    create a new integer first; there isn't a good reason to use a while loop instead of a
    for loop in this case, but it's good practice...)
    
    3. Write a function doubles() that takes one number as its input and doubles that
    number three times using a loop, displaying each result on a separate line; test your
    function by calling doubles(2) to display 4, 8, and 16
    
    Copyright 2016 Real Python. All rights reserved.

---
+++ review_exercise_p59.py
@@ -0,0 +1,19 @@
+# Real-Python-book
+
+# Review exercises for Page 59
+
+
+# F O R   L O O P   E X E R C I S E 
+
+for n in range(2, 11):
+	print("n =", n)
+
+# D O U B L E S   F U N C T I O N   E X E R C I S E 
+
+def doubles(n):
+	while (n < 16):
+		n = n * 2
+		print("n =", n)
+print("Loop Finished")
+
+doubles(2)
